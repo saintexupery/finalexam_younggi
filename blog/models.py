73b02f3eb65f6399b01b187ed36fc242aeea79ef
Younggi Min <younggimin91@gmail.com>
2016-06-08 14:58:05 +0900

    shop 모델을 추가하고 마이그레이트 적용

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Category(models.Model):
     category = models.CharField(max_length=100)
@@ -7,3 +8,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100)
+    phonenumber = PhoneNumberField()
+    address = models.TextField()
+    content = models.TextField()
+    photo1 = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
