912c1c6020b9ebe0cf32d096087b65ac147ebb92
Younggi Min <younggimin91@gmail.com>
2016-06-08 16:17:40 +0900

    blog.Review 등록시에 shop과 user필드를 코드를 통해 등록

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Category, Shop
 from .forms import ReviewForm
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     category_list = Category.objects.all()
@@ -15,11 +16,15 @@ def shop_detail(request, pk):
         'shop': shop,
     })
 
+@login_required
 def review_new(request, shop_pk):
     if request.method == 'POST':
         form = ReviewForm(request.POST)
         if form.is_valid():
-            review = form.save()
+            review = form.save(commit=False)
+            review.shop = get_object_or_404(Shop, pk=shop_pk)
+            review.user = request.user
+            review.save()
             return redirect('blog:shop_detail', shop_pk)
     else:
         form = ReviewForm()
