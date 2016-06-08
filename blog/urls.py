647df74b2c043a5bb3bd09cf835c3320f988b115
Younggi Min <younggimin91@gmail.com>
2016-06-08 15:28:52 +0900

    shop과 shop_detail간의 링크생성

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^(?P<pk>\d+)/$', views.shop_detail),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<pk>\d+)/$', views.shop_detail, name='shop_detail'),
 ]
