f27eee975735bf24b15ef3bbe2db4a3249591491
Younggi Min <younggimin91@gmail.com>
2016-06-08 15:17:14 +0900

    blog.Shop을 인덱스 뷰에 적용

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.contrib.humanize.templatetags.humanize import intcomma
-from .models import Category
+from .models import Category, Shop
 
-class PostAdmin(admin.ModelAdmin):
+class CategoryAdmin(admin.ModelAdmin):
     list_display = ['category', 'created_at']
 
-admin.site.register(Category, PostAdmin)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ['name', 'address', 'content_size','created_at']
+
+    def content_size(self, post):
+        return '%s자' % intcomma(len(shop.content))
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Shop, ShopAdmin)
--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from .models import Category, Shop
 
 def index(request):
     category_list = Category.objects.all()
+    shop_list = Shop.objects.all()
     return render(request, 'blog/index.html', {
-        'category_list':category_list,
+        'category_list':category_list, 'shop_list': shop_list,
     })
