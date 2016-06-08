10bd3dea79fd77b173d2c6428515466f70b518cd
Younggi Min <younggimin91@gmail.com>
2016-06-08 14:40:44 +0900

    Category 모델 추가 / 마이그레이션 적용

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    category = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
