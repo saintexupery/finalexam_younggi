35ce26144fda6d2243412fffb1bc78dccbea79c8
Younggi Min <younggimin91@gmail.com>
2016-06-08 14:50:41 +0900

    blog.Category 모델을 admin에 추가

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
+from .models import Category
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['category', 'created_at']
+
+admin.site.register(Category, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Category(models.Model):
     category = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.category
